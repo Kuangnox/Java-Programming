cefb188a926fe3f1e7ef8fe9af39545e4a1146c7
Kuangnox <13549522933@163.com>
2024-09-22 19:46:42 +0800

    Update program  7.java

--- Chapter 1/program  7.java
+++ Chapter 1/program  7.java
@@ -1,21 +1,20 @@
 public class Approximate {
     public static void main(String[] args) {
-        double pai=0.0;//近似值的和
-        double sum=0.0;//一直在变的分数
-        double flag;//正反数
-        double common=4.0;//常数4
-        double i=1.0;//逐渐增大的分母
-        double b=1.0;
-        for(i=1.0;i<=11;i=i+2){
-            flag=1/i;
-            if(i%4==3){
-            flag=-flag;
+        double result1 = 0;
+        double result2 = 0;
+        int flag = 1;
+        for ( double i = 1 ; i <= 13 ; i=i+2 ){
+            if(i <= 11){
+                result1+=flag * (1/i);
+                result2+=flag * (1/i);
+            }else{
+                result2+=flag * (1/i);
             }
-            sum=sum+flag;
+            flag = flag * (-1);
         }
-        pai=(pai+sum)*common;
-        System.out.println(pai);
-        pai=pai+b/13;
-        System.out.println(pai);
+        result1 = 4 * result1;
+        result2 = 4 * result2;
+        System.out.println("result1 = " + result1) ;
+        System.out.println("result2 = " + result2) ;
     }
 }
