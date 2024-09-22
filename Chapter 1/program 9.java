a596891ecb9bc1fed950df133622ec4237ca719f
Kuangnox <13549522933@163.com>
2024-09-22 19:52:23 +0800

    Update program 9.java

--- Chapter 1/program 9.java
+++ Chapter 1/program 9.java
@@ -3,7 +3,7 @@ public class AreaAndPerimeterOfARectangle {
         double perimeter;//周长
         double area;//面积
         double width=4.5,height=7.9;
-        perimeter=2*width*height;
+        perimeter=2*(width+height);
         area = width * height;
         System.out.println(perimeter);
         System.out.println(area);
